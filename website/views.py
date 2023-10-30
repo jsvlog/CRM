f7aa687a33bd1d84da9c632c47f11be97511cc00
jsvlog <lizardojohnsonc@gmail.com>
2023-10-30 10:14:14 +0800

    register

--- website/views.py
+++ website/views.py
@@ -25,3 +25,9 @@ def logout_user(request):
     logout(request)
     messages.success(request, "You have been logout!")
     return redirect('home')
+
+def register_user(request):
+    return render(request, 'register.html', {})
+
+
+
