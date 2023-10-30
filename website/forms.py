618a789865728ecd7c670332c20cdcb37411c5a1
jsvlog <lizardojohnsonc@gmail.com>
2023-10-30 19:59:06 +0800

    reg form

--- website/forms.py
+++ website/forms.py
@@ -5,7 +5,7 @@ from django import forms
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'email address'}) )
     first_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'first name'}) )
-    last_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'first name'}))
+    last_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'last name'}))
 
 
     class Meta:
