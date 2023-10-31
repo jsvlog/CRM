19564e1e443be99176ad869a1fed0d8e6d124b51
jsvlog <lizardojohnsonc@gmail.com>
2023-10-31 19:14:01 +0800

    individual record

--- website/views.py
+++ website/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 # Create your views here.
 def home(request):
+    records = Record.objects.all()
+
     #Check to see if logging in
     if request.method == 'POST':
         username = request.POST['username']
@@ -19,7 +22,7 @@ def home(request):
             messages.success(request, "there was an error logging in!")
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records':records})
 
 
 def logout_user(request):
@@ -45,3 +48,11 @@ def register_user(request):
     return render(request, 'register.html', {'form':form})
 
 
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        #look up records
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record':customer_record})
+    else:
+        messages.success(request, 'You must be logged in to view record')
+        return redirect('home')
